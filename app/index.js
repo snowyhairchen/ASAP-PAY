b877b851f3c62ef843c85d56081c056b14b7efdf
snowyhair <williamchen19951221@gmail.com>
2017-08-25 18:44:30 +0800

    登入頁面UI完成

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+import Navigator from './config/routes';
 
 EStyleSheet.build({
-  $primaryColor: '#4F6D7A'
+  $primaryColor: '#FFFFFF'
 });
 
-export default () => <Home />;
+export default () => <Navigator />;
